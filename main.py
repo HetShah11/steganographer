96b147cab437c6b516403af4523b9dd7e7e54ab7
HetShah11 <arihanthet11@gmail.com>
2022-02-07 15:23:03 +0530

    made some changes to encode and decode func

--- main.py
+++ main.py
@@ -1,101 +1,101 @@
-import os
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
+import cv2
 
-DIRECTORY_PATH = os.path.dirname(os.path.realpath(__file__))
+DELIMITER = "$t3G4##"
 
-def encode(src, message):
-    """
-    :param src: source name of the image file
-    :param message: message to be encrypted
-    :return: none
-    """
-    # opening the image for reading
-    img = Image.open(DIRECTORY_PATH + "/images/" + src, 'r')
-    width, height = img.size
-    data_arr = np.array(list(img.getdata()))
-    # checking mode of img if its RGB or RGBA
-    # n is the no. of bytes in a pixel
-    if img.mode == 'RGB':
-        n = 3
-    elif img.mode == 'RGBA':
-        n = 4
-    # calculating total_pixels
-    total_pixels = len(data_arr)//n
-
-    # adding a delimiter to show end of message
-    message += '$t3g0'
-    # converting message to binary
-    b_message = ''.join([format(ord(i), "08b") for i in message])
-    req_pixels = len(b_message)
-
-    # checking the required pixels are available or not
-    if req_pixels > total_pixels:
-        print("Error message cannot be steganographed due to size constraints!")
-        return
+def to_binary(data):
+    """Convert `data` to binary format as string"""
+    if type(data) == str:
+        return ''.join([ format(ord(i), "08b") for i in data ])
+    elif type(data) == bytes or type(data) == np.ndarray:
+        return [ format(i, "08b") for i in data ]
+    elif type(data) == int or type(data) == np.uint8:
+        return format(data, "08b")
     else:
-        index = 0
-        # iterating over the image pixels
-        for pix in range(total_pixels):
-            # iterating over the color bytes (R, G, B) of each pixel
-            for byte in range(3):
-                if req_pixels > index:
-                    # editing the last bit with b_message
-                    data_arr[pix][byte] = int(bin(data_arr[pix][byte])[2:-1] + b_message[index], 2)
-                    index += 1
-        # reshaping the new data
-        en_data = data_arr.reshape(height, width, n)
-        # creating new image from en_data
-        plt.imsave(DIRECTORY_PATH+"/steganographed_img/"+src, en_data.astype('uint8'))
-
-        # en_img = Image.fromarray(en_data).save(DIRECTORY_PATH+"/steganographed_img/"+src)
-        print("Image Encoded Successfully")
+        raise TypeError("Error in binary conversion!!")
 
 
-def decode(src):
-    """
-    :param src: source path of file to be decrypted
-    :return: decrypted message
-    """
-    # opening the image for reading
-    img = Image.open(DIRECTORY_PATH + "/steganographed_img/" + src, 'r')
-    data_arr = np.array(list(img.getdata()))
-    # checking mode of img if its RGB or RGBA
-    # n is the no. of bytes in a pixel
-    if img.mode == 'RGB':
-        n = 3
-    elif img.mode == 'RGBA':
-        n = 4
-    # calculating total_pixels
-    total_pixels = len(data_arr) // n
+def encode(image, plaintext):
+    # read the image
+    img = cv2.imread(image)
+    # maximum bytes to encode
+    n_bytes = img.shape[0] * img.shape[1] * 3 // 8
+    print("Maximum bytes to encode:", n_bytes)
+    if len(plaintext) > n_bytes:
+        raise ValueError("Error!! Insufficient bytes, need bigger image or less data.")
 
-    message = ""
-    hidden_bits = ""
-    # iterating over the image pixels
-    for pix in range(total_pixels):
-        # iterating over the color bytes (R, G, B) of each pixel
-        for byte in range(3):
-            hidden_bits += (bin(data_arr[pix][byte])[2:][-1])
-            # b_message.append(bin(data_arr[pix][byte])[-1])
+    # stopping criteria
+    plaintext += DELIMITER
+    data_index = 0
+    # convert data to binary
+    binary_pt = to_binary(plaintext)
+    # size of data to hide
+    data_len = len(binary_pt)
 
-    # making groups of 8
-    hidden_bits = [hidden_bits[i:i + 8] for i in range(0, len(hidden_bits), 8)]
-    # b_message = [b_message[i:i + 8] for i in range(0, len(b_message), 8)]
-    # converting it into ASCII again
-    for i in range(len(hidden_bits)):
-        if message[-5:] == "$t3g0":
+    for row in img:
+        for pixel in row:
+            # convert RGB values to binary format
+            r, g, b = to_binary(pixel)
+            # modify the least significant bit only if there is still data to store
+            if data_index < data_len:
+                # red pixel bit
+                pixel[0] = int(r[:-1] + binary_pt[data_index], 2)
+                data_index += 1
+            if data_index < data_len:
+                # green pixel bit
+                pixel[1] = int(g[:-1] + binary_pt[data_index], 2)
+                data_index += 1
+            if data_index < data_len:
+                # blue pixel bit
+                pixel[2] = int(b[:-1] + binary_pt[data_index], 2)
+                data_index += 1
+            # if data is encoded
+            if data_index >= data_len:
+                break
+    return img
+
+def decode(image):
+    print("[+] Decoding...")
+    # read the image
+    img = cv2.imread(image)
+    decoded_message = ""
+    for row in img:
+        for pixel in row:
+            r, g, b = to_binary(pixel)
+            decoded_message += r[-1]
+            decoded_message += g[-1]
+            decoded_message += b[-1]
+
+    # split by 8-bits
+    all_bytes = [decoded_message[i: i + 8] for i in range(0, len(decoded_message), 8)]
+    # convert from bits to characters
+    pt = ""
+    for byte in all_bytes:
+        pt += chr(int(byte, 2))
+        if pt[-5:] == DELIMITER:
             break
-        else:
-            message += chr(int(hidden_bits[i], 2))
-            # message += chr(int(''.join(b_message[i]), 2))
-    # checking the presence of delimiter
-    print("\n##################\n")
-    if "$t3g0" in message:
-        print("\tSuccessfully Decrypted \n\tThe Message is - ", message[:-5])
-    else:
-        print("\tNo Hidden Message Found")
+    return pt[:-5]
 
+
+def encrypt():
+    image_name = input("Enter image name(with extension): ")
+    image = cv2.imread(image_name)
+
+    data = input("Enter data to be encoded : ")
+    if (len(data) == 0):
+        raise ValueError('Error...Data is empty!!')
+
+    filename = input("Enter the name of new encoded image(with extension): ")
+    encoded_image = encode(image,data)
+    cv2.imwrite(filename, encoded_image)
+
+def decrypt():
+    image_name = input("Enter the name of the steganographed image that you want to decode (with extension) :")
+    image = cv2.imread(image_name)  # read the image using cv2.imread()
+
+    print("The Steganographed image is as shown below: ")
+    text = decode(image)
+    return text
 
 def main():
     while True:
@@ -106,15 +106,13 @@ def main():
                            "2.Decode image\n"
                            "3.Exit"
                            "\nEnter your choice: "))
-
-
         if choice == 1:
-            src = input("\n\tEnter name of the source file: ")
-            message = input("\tEnter message to be hidden in image: ")
-            encode(src, message)
+            print("\nEncoding....")
+            encrypt()
+
         elif choice == 2:
-            src = input("\n\tEnter name of the source file: ")
-            decode(src)
+            print("\nDecoding....")
+            print("Decoded message is " + decode_text())
         else:
             return
 
